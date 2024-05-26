cfaab62a5e2d19a3c8499de46b0d9017351f285f
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-05-26 18:14:46 +0300

    Saving Basics

--- yumm/src/App.js
+++ yumm/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>Tasty Delights, a click away.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Yumm
         </a>
       </header>
     </div>
