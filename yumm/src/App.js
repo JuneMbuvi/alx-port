1c688c993d00d090c2fd108811b96b5b6857f246
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-05-26 19:15:19 +0300

    adding the header and search bar

--- yumm/src/App.js
+++ yumm/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Yumm</h1>
         <p>
-          Edit <code>src/App.js</code>Tasty Delights, a click away.
+          Tasty Delights, a click away.
         </p>
         <a
           className="App-link"
@@ -23,3 +24,4 @@ function App() {
 }
 
 export default App;
+
