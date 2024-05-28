61c5f690cec94140ba0227fb056fc80d7a93db80
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-05-28 08:33:06 +0300

    Header

---
+++ yumm/src/Header.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Yumm</h1>
+      <input type="text" placeholder="Search recipes..." />
+    </header>
+  );
+}
+
+export default Header;
