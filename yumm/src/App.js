e3661e708f77d4d599d988614fb8472e54919079
SherrynneJoy <sherrynnembuvi@gmail.com>
2024-05-29 20:43:26 +0300

    almost done

--- yumm/src/App.js
+++ yumm/src/App.js
@@ -1,55 +1,57 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import RecipeList from './RecipeList';
-import RecipeDetail from './RecipeDetail';
-import CategoryFilter from './CategoryFilter';
+import React, { useEffect, useState } from 'react'
+import './App.css'; 
+import Recipe from './Recipe'; 
 
-class App extends Component {
-  state = {
-    recipes: [],
-    selectedRecipe: null,
-    selectedCategory: '',
-    categories: [],
-  };
+const App = () => { 
+const APP_ID = 'c6b7580a'; 
+const APP_KEY = 'ac1c5c75fd245a58c44a19dfe1501608	'; 
+const [recipes, setRecipes] = useState([]); 
+const [search, setSearch] = useState(""); 
+const [query, setQuery] = useState("chicken"); 
+useEffect(() => { 
+	getRecipes(); 
+}, [query]) 
+const getRecipes = async () => { 
+	const response = await fetch 
+		(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`); 
+	const data = await response.json(); 
+	setRecipes(data.hits); 
+	// console.log(data); 
 
-  componentDidMount() {
-  axios.get('alx-port/yumm/public/recipes.json')
-      .then(response => {
-        const categories = [...new Set(response.data.map(recipe => recipe.category))];
-        this.setState({ recipes: response.data, categories });
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  }
+}; 
+const updateSearch = e => { 
+	setSearch(e.target.value); 
+}; 
+const getSearch = e => { 
+	e.preventDefault(); 
+	setQuery(search); 
+	setSearch(""); 
+} 
 
-  handleCategoryChange = (category) => {
-    this.setState({ selectedCategory: category });
-  };
+return ( 
+	<div className="App"> 
+	<form className="search-form" onSubmit={getSearch} > 
+		<input className="search-bar" type="text" value={search} 
+			onChange={updateSearch} /> 
+		<button className="search-button" type="submit" > 
+			Search 
+		</button> 
+	</form> 
+	<div className="recipes"> 
+		{recipes.map(recipe => ( 
+		<Recipe 
+			key={recipe.recipe.label} 
+			title={recipe.recipe.label} 
+			calories={recipe.recipe.calories} 
+			image={recipe.recipe.image} 
+			ingredients={recipe.recipe.ingredients} 
+		/> 
 
-  handleRecipeSelect = (recipe) => {
-    this.setState({ selectedRecipe: recipe });
-  };
+		))} 
+	</div> 
 
-  render() {
-    const { recipes, selectedRecipe, selectedCategory, categories } = this.state;
-    const filteredRecipes = selectedCategory
-      ? recipes.filter(recipe => recipe.category === selectedCategory)
-      : recipes;
+	</div> 
+); 
+} 
 
-    return (
-      <div>
-        <CategoryFilter
-          categories={categories}
-          selectedCategory={selectedCategory}
-          onCategoryChange={this.handleCategoryChange}
-        />
-        <RecipeList
-          recipes={filteredRecipes}
-          onSelectRecipe={this.handleRecipeSelect}
-        />
-        <RecipeDetail recipe={selectedRecipe} />
-      </div>
-    );
-  }
-}
-
-export default App;
+export default App; 
